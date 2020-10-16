99ff87ea7f486577f3b4be3c7c8766c30a582745
Alexandra Kissel <akissel@wisc.edu>
2020-10-15 20:51:09 -0500

    trying to solve concatenation issues

--- src/newton_raphson.py
+++ src/newton_raphson.py
@@ -9,14 +9,18 @@ def get_vals(body_i, body_j, gcon, t):
     # check for ground body
     if body_j.body_id == 0:
         # no r, p for ground body
-        Phi = np.concatenate((gcon.phi(t), 0.5*body_i.p_i.T @ body_i.p_i - 0.5), axis=0)
-        Phi_q = np.concatenate((gcon.partial_r, gcon.partial_p, body_i.p_i.T), axis=0)
-        q = np.concatenate((body_i.r_i, body_i.p_i), axis=0)
+        Phi = np.concatenate((gcon.phi(t), 0.5*body_i.p.T @ body_i.p - 0.5), axis=None)
+        Phi_q_g = np.concatenate((gcon.partial_r(), gcon.partial_p()), axis=None)
+        print(Phi_q_g)
+        print(body_i.p.T)
+        Phi_q = np.concatenate((Phi_q_g, body_i.p.T), axis=1)
+        q = np.concatenate((body_i.r, body_i.p), axis=None)
     else:
-        Phi = np.concatenate((gcon.phi(t), 0.5*body_i.p_i.T @ body_i.p_i - 0.5, 0.5*body_j.p_j.T @ body_j.p_j - 0.5),
-                             axis=0)
-        Phi_q = np.concatenate((gcon.partial_r, gcon.partial_p, body_i.p_i.T, body_j.p_j.T))
-        q = np.concatenate((body_i.r_i, body_i.p_i, body_j.r_j, body_j.p_j), axis=0)
+        Phi = np.concatenate((gcon.phi(t), 0.5*body_i.p.T @ body_i.p - 0.5, 0.5*body_j.p.T @ body_j.p - 0.5),
+                             axis=None)
+        Phi_q_g = np.concatenate((gcon.partial_r(), gcon.partial_p()), axis=None)
+        Phi_q = np.concatenate((Phi_q_g, body_i.p.T, body_j.p.T), axis=1)
+        q = np.concatenate((body_i.r, body_i.p, body_j.r, body_j.p), axis=None)
 
     return q, Phi, Phi_q
 
@@ -38,13 +42,13 @@ def newton_raphson(body_i, body_j, gcon, t, tol):
 
         # update body attributes
         if body_j.body_id == 0:
-            body_i.r_i = q_new[0:3]
-            body_i.p_i = q_new[3:]
+            body_i.r = q_new[0:3]
+            body_i.p = q_new[3:]
         else:
-            body_i.r_i = q_new[0:3]
-            body_i.p_i = q_new[3:7]
-            body_j.r_j = q_new[7:10]
-            body_j.p_j = q_new[10:]
+            body_i.r = q_new[0:3]
+            body_i.p = q_new[3:7]
+            body_j.r = q_new[7:10]
+            body_j.p = q_new[10:]
 
         # Get updated Phi, Phi_q
         q, Phi, Phi_q = get_vals(body_i, body_j, gcon, t)
