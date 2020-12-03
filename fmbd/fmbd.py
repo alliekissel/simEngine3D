69c5f769301b23ae8326912d911cfbad31baf5b8
Alexandra Kissel <127-akissel@users.noreply.newton.msvc.wisc.edu>
2020-12-02 19:56:45 -0600

    Add fmbd functions for HW10

--- fmbd/fmbd.py
+++ fmbd/fmbd.py
@@ -156,6 +156,68 @@ def get_Q_gravity(rho=7700, W=0.003, H=0.003, L=0.5):
     return rho * Q
 
 
+def get_Q_internal_e(e, W=0.003, H=0.003, L=0.5, E=2.0e11, nu=0.3):
+    # element of the jacobian for the internal force vector
+
+    # 2 GQ points for i, j (eta, zeta)
+    W_i = [1, 1]
+    W_j = W_i
+    # 4 GQ points for k (xi)
+    W_k = [0.3479, 0.6521, 0.6521, 0.3479]
+
+    X_i = [-np.sqrt(3)/3, np.sqrt(3)/3]
+    X_j = X_i
+    X_k = [-0.8611, -0.3399, 0.3399, 0.8611]
+
+    k1 = 10 * ((1 + nu) / (12 + 11 * nu))
+    k2 = k1
+    D = E * nu / ((1 + nu) * (1 - 2 * nu)) * np.array([[(1 - nu) / nu, 1, 1, 0, 0, 0],
+                                                       [1, (1 - nu) / nu, 1, 0, 0, 0],
+                                                       [1, 1, (1 - nu) / nu, 0, 0, 0],
+                                                       [0, 0, 0, (1 - 2 * nu) / (2 * nu), 0, 0],
+                                                       [0, 0, 0, 0, (1 - 2 * nu) / (2 * nu) * k1, 0],
+                                                       [0, 0, 0, 0, 0, (1 - 2 * nu) / (2 * nu) * k2]])
+
+    e_0 = np.array([[0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, L, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]]).T
+    Q_e = np.zeros((24, 24))
+    Q_inner_sum = np.zeros((24, 24))
+    for i in range(2):
+        for j in range(2):
+            for k in range(4):
+                J_inv = np.linalg.inv(np.concatenate((get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
+                                                      get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
+                                                      get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0), axis=1))
+                S_1_F = J_inv[0, 0] * get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[1, 0] * get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[2, 0] * get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i])
+                S_2_F = J_inv[0, 1] * get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[1, 1] * get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[2, 1] * get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i])
+                S_3_F = J_inv[0, 2] * get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[1, 2] * get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) \
+                        + J_inv[2, 2] * get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i])
+                S_F = np.concatenate((S_1_F, S_2_F, S_3_F), axis=1)
+                for l in range(3):
+                    for m in range(3):
+                        Q_inner_sum += D[l, m] * (1/2 * (S_F[:, 24 * l:24 * l + 24].T @ S_F[:, 24 * l:24 * l + 24]) * (
+                                      e.T @ S_F[:, 24 * m:24 * m + 24].T @ S_F[:, 24 * m:24 * m + 24] @ e - 1)
+                                                  + (S_F[:, 24 * l:24 * l + 24].T @ S_F[:, 24 * l:24 * l + 24] @ e
+                                                     @ e.T @ S_F[:, 24 * m:24 * m + 24].T @ S_F[:, 24 * m:24 * m + 24]))
+                Q_e += W_i[i] * W_j[j] * W_k[k] \
+                     * (Q_inner_sum
+                        + (D[3, 3] * (S_2_F.T @ S_3_F + S_3_F.T @ S_2_F) * (e.T @ S_2_F.T @ S_3_F @ e))
+                        + (D[3, 3] * (S_2_F.T @ S_3_F + S_3_F.T @ S_2_F) @ e @ e.T @ (S_2_F.T @ S_3_F + S_3_F.T @ S_2_F))
+                        + (D[4, 4] * (S_1_F.T @ S_3_F + S_3_F.T @ S_1_F) * (e.T @ S_1_F.T @ S_3_F @ e))
+                        + (D[4, 4] * (S_1_F.T @ S_3_F + S_3_F.T @ S_1_F) @ e @ e.T @ (S_1_F.T @ S_3_F + S_3_F.T @ S_1_F))
+                        + (D[5, 5] * (S_1_F.T @ S_2_F + S_2_F.T @ S_1_F) * (e.T @ S_1_F.T @ S_2_F @ e))
+                        + (D[5, 5] * (S_1_F.T @ S_2_F + S_2_F.T @ S_1_F) @ e @ e.T @ (S_1_F.T @ S_2_F + S_2_F.T @ S_1_F)) ) \
+                     * np.linalg.det(np.concatenate((get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
+                                                     get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
+                                                     get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0), axis=1))
+
+    return -Q_e
+
+
 def get_Q_internal(e, W=0.003, H=0.003, L=0.5, E=2.0e11, nu=0.3):
     # e is a 24x1 time dependent vector
     # 3 GQ points for i, j (eta, zeta)
@@ -198,13 +260,13 @@ def get_Q_internal(e, W=0.003, H=0.003, L=0.5, E=2.0e11, nu=0.3):
                 S_F = np.concatenate((S_1_F, S_2_F, S_3_F), axis=1)
                 for l in range(3):
                     for m in range(3):
-                        Q_inner_sum += (S_F[:, 24*l:24*l+24].T @ S_F[:, 24*l:24*l+24] @ e) \
-                         @ (D[l, m]/2 * (e.T @ S_F[:, 24*m:24*m+24].T @ S_F[:, 24*m:24*m+24] @ e - 1))
+                        Q_inner_sum += ((S_F[:, 24*l:24*l+24].T @ S_F[:, 24*l:24*l+24]) @ e) \
+                         @ (D[l, m]/2 * (e.T @ (S_F[:, 24*m:24*m+24].T @ S_F[:, 24*m:24*m+24]) @ e - 1))
                 Q += W_i[i] * W_j[j] * W_k[k] \
                      * (Q_inner_sum
-                     + ((S_2_F.T @ S_3_F + S_3_F.T @ S_2_F) @ e) @ (D[3, 3] * (e.T @ S_2_F.T @ S_3_F @ e))
-                     + ((S_1_F.T @ S_3_F + S_3_F.T @ S_1_F) @ e) @ (D[4, 4] * (e.T @ S_1_F.T @ S_3_F @ e))
-                     + ((S_1_F.T @ S_2_F + S_2_F.T @ S_1_F) @ e) @ (D[5, 5] * (e.T @ S_1_F.T @ S_2_F @ e))) \
+                     + ((S_2_F.T @ S_3_F + S_3_F.T @ S_2_F) @ e) * (D[3, 3] * (e.T @ S_2_F.T @ S_3_F @ e))
+                     + ((S_1_F.T @ S_3_F + S_3_F.T @ S_1_F) @ e) * (D[4, 4] * (e.T @ S_1_F.T @ S_3_F @ e))
+                     + ((S_1_F.T @ S_2_F + S_2_F.T @ S_1_F) @ e) * (D[5, 5] * (e.T @ S_1_F.T @ S_2_F @ e))) \
                      * np.linalg.det(np.concatenate((get_S_xi(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
                                                      get_S_eta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0,
                                                      get_S_zeta(L, W, H, X_k[k], X_j[j], X_i[i]) @ e_0), axis=1))
@@ -227,8 +289,19 @@ def get_Q_external(f_n_applied, n=1, W=0.003, H=0.003, L=0.5, xi=1, eta=0, zeta=
 
     return Q
 
+def get_jacobian(e):
+    zero_block = np.zeros((24, 24))
+    identity_block = np.eye(24, 24)
+    nonzero_block = get_M() - get_Q_internal_e(e)
+
+    return np.block([
+        [nonzero_block, identity_block],
+        [identity_block, zero_block]
+    ])
+
 # testing
 def main():
+    # ========================= HOMEWORK 9 TESTING ========================
     #print(get_S(1,1,1,1,1,1))
     #print(get_S_zeta(1,1,1,1,1,1))
     #print(get_M().diagonal())
@@ -260,6 +333,12 @@ def main():
     # plt.savefig("2D")
 
     #print(get_Q_external(np.array([[10 * np.cos(45 * np.pi/180), 0, 10 * np.sin(45 * np.pi/180)]]).T))
+
+    # ========================= HOMEWORK 10 TESTING ========================
+
+    e = np.array([[0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0.5, 0, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0]]).T
+    #print(get_Q_internal((np.array([[0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0.5, 0, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0]]).T)))
+    #print(np.linalg.inv(get_jacobian(e)))
 
 if __name__ == '__main__':
     main()
